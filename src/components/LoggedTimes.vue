e7ec93166be92a62704c477d650385229b6bf744
Lucas Gomes <lukas_g_s_@hotmail.com>
2024-10-22 16:41:18 -0300

    feat: adds icons to buttons

--- src/components/LoggedTimes.vue
+++ src/components/LoggedTimes.vue
@@ -1,20 +1,20 @@
 <template>
     <div class="flex flex-col items-center justify-center ">
-        <h3 class="text-4xl font-medium mb-4 text-text">Logged Times</h3>
         <div v-if="timerStore.loggedTimes.length >= 1"
             class="relative flex flex-col w-full h-full overflow-scroll text-gray-700 bg-background bg-clip-border">
-            <div class="overflow-y-auto max-h-80 w-full">
+            <h3 class="text-2xl font-semibold mb-4 text-text">Logged Times</h3>
+            <div class="overflow-y-auto max-h-96 w-full">
                 <table class="w-full text-center table-auto min-w-max">
                     <thead>
                         <tr class="border-b border-slate-300 bg-background">
-                            <th id='table-header-date' class="p-4 text-sm font-normal leading-none text-text">#
+                            <th id='table-header-date' class="p-4 text-sm font-semibold leading-none text-text">#
                             </th>
-                            <th id='table-header-date' class="p-4 text-sm font-normal leading-none text-text">Date
+                            <th id='table-header-date' class="p-4 text-sm font-semibold leading-none text-text">Date
                             </th>
-                            <th id='table-header-time' class="p-4 text-sm font-normal leading-none text-text">
+                            <th id='table-header-time' class="p-4 text-sm font-semibold leading-none text-text">
                                 Logged
                                 Time</th>
-                            <th id='table-header-break' class="p-4 text-sm font-normal leading-none text-text">
+                            <th id='table-header-break' class="p-4 text-sm font-semibold leading-none text-text">
                                 Break
                                 Time</th>
                         </tr>
@@ -22,21 +22,21 @@
                     <tbody>
                         <tr v-for="(log, idx) in timerStore.loggedTimes" :key="idx" class="text-center">
                             <td class="p-4 border-b border-slate-200 py-5">
-                                <p :id="`table-number-${idx}`" class="block font-semibold text-sm text-text">{{
+                                <p :id="`table-number-${idx}`" class="block font-light text-sm text-text">{{
                                     idx + 1
-                                }}</p>
+                                    }}</p>
                             </td>
                             <td class="p-4 border-b border-slate-200 py-5">
-                                <p :id="`table-date-${idx}`" class="block font-semibold text-sm text-text">{{
+                                <p :id="`table-date-${idx}`" class="block font-light text-sm text-text">{{
                                     log.date
-                                }}</p>
+                                    }}</p>
                             </td>
                             <td class="p-4 border-b border-slate-200 py-5">
-                                <p :id="`table-total-time-${idx}`" class="block font-semibold text-sm text-text">{{
+                                <p :id="`table-total-time-${idx}`" class="block font-light text-sm text-text">{{
                                     log.totalTime }}</p>
                             </td>
                             <td class="p-4 border-b border-slate-200 py-5">
-                                <p :id="`table-break-time-${idx}`" class="block font-semibold text-sm text-text">{{
+                                <p :id="`table-break-time-${idx}`" class="block font-light text-sm text-text">{{
                                     log.breakTime != null ?
                                         log.breakTime : "No break time" }}</p>
                             </td>
@@ -50,8 +50,11 @@
         </section>
 
         <div v-if="timerStore.loggedTimes.length >= 1" class="mt-4">
-            <button @click="timerStore.clearLogs" class="bg-orange-500 text-white px-4 py-2 rounded mr-2"
-                v-if="!timerStore.isRunning">
+            <button @click="timerStore.clearLogs" class="flex gap-2 bg-orange-500 text-white px-4 py-2 rounded mr-2"
+                v-if="!timerStore.isRunning || !timerStore.onBreak">
+                <i>
+                    <img src="@/assets/trashBin.svg" class="h-6 w-6" />
+                </i>
                 Clear data
             </button>
         </div>
--- src/components/MainTimer.vue
+++ src/components/MainTimer.vue
@@ -1,5 +1,5 @@
 <template>
-    <div class="flex flex-col items-center justify-center my-20">
+    <div class="flex flex-col items-center justify-center my-10">
         <h1 class="text-4xl font-bold mb-4 text-text">Study Timer</h1>
         <div class="text-9xl font-mono text-text" :class="{ 'flash': timerStore.onBreak }">{{ timerStore.formattedTime
             }}</div>
@@ -8,8 +8,11 @@
         }}
         </div>
         <div class="flex mt-4 min-w-full justify-center items-center">
-            <button id="start-button" @click="timerStore.start" class="bg-secondary text-white px-4 py-2 rounded mr-2"
-                v-if="!timerStore.isRunning">
+            <button id="start-button" @click="timerStore.start"
+                class="flex gap-2 bg-secondary text-white px-4 py-2 rounded mr-2" v-if="!timerStore.isRunning">
+                <i>
+                    <img src="@/assets/playPause.svg" class="h-6 w-6" />
+                </i>
                 {{ timerStore.onBreak ? 'Finish break' : 'Start' }}
             </button>
             <!-- <button @click="timerStore.pause" class="bg-yellow-500 text-white px-4 py-2 rounded mr-2"
@@ -17,11 +20,17 @@
                 Pause
             </button> -->
             <button id="take-a-break-button" @click="timerStore.takeBreak"
-                class="bg-orange-500 text-white px-4 py-2 rounded mr-2" v-if="timerStore.isRunning">
+                class="flex gap-2 bg-orange-500 text-white px-4 py-2 rounded mr-2" v-if="timerStore.isRunning">
+                <i>
+                    <img src="@/assets/coffee.svg" class="h-6 w-6" />
+                </i>
                 Take a break
             </button>
             <button id="log-time-button" v-if="timerStore.isRunning || timerStore.onBreak" @click="timerStore.reset"
-                class="bg-red-500 text-white px-4 py-2 rounded">
+                class="flex gap-2 bg-red-500 text-white px-4 py-2 rounded">
+                <i>
+                    <img src="@/assets/clockCheckmark.svg" class="h-6 w-6" />
+                </i>
                 Log time
             </button>
         </div>
